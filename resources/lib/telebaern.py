0a3715338f34230d070a9f381d511207f7800cbd
Alexander Seiler <seileralex@gmail.com>
2018-10-26 02:00:13 +0200

    Simplify and PEP 8

--- resources/lib/telebaern.py
+++ resources/lib/telebaern.py
@@ -19,54 +19,21 @@
 # along with plugin.video.telebaern.
 # If not, see <http://www.gnu.org/licenses/>.
 
-# import sys
-# import traceback
-
-# import json
-# import socket
-# import urllib
-# import urlparse
-
-import xbmc
-# import xbmcgui
-# import xbmcplugin
 import xbmcaddon
-
 import azmedien
-
-def get_boolean_setting(label):
-    """
-    Retrieve the boolean value of a setting switch.
-
-    Keyword arguments:
-    label -- the settings label
-    """
-    return REAL_SETTINGS.getSetting(label) == 'true'
-
 
 ADDON_ID = 'plugin.video.telebaern'
 REAL_SETTINGS = xbmcaddon.Addon(id=ADDON_ID)
-ADDON_NAME = REAL_SETTINGS.getAddonInfo('name')
-ADDON_VERSION = REAL_SETTINGS.getAddonInfo('version')
 ICON = REAL_SETTINGS.getAddonInfo('icon')
 FANART = REAL_SETTINGS.getAddonInfo('fanart')
-LANGUAGE = REAL_SETTINGS.getLocalizedString
-PROFILE = xbmc.translatePath(
-    REAL_SETTINGS.getAddonInfo('profile')).decode("utf-8")
 HOST = 'telebaern.tv'
-# DEBUG = REAL_SETTINGS.getSetting('Enable_Debugging') == 'true'
-
-
-# socket.setdefaulttimeout(TIMEOUT)
-
-
-# General helper functions:
 
 
 def run():
     """
     Run the plugin.
     """
-    customer = azmedien.CustomerAddon(addon_id=ADDON_ID, real_settings=REAL_SETTINGS, icon=ICON, fanart=FANART)
+    customer = azmedien.CustomerAddon(addon_id=ADDON_ID,
+                                      real_settings=REAL_SETTINGS,
+                                      icon=ICON, fanart=FANART)
     azmedien.run(customer, host=HOST)
-    
